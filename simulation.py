dcee63cf326ce3f2e15deceedc0be37544849d59
Will Xue <xuewilliam5@gmail.com>
2023-09-23 17:26:47 -0400

    format output

--- simulation.py
+++ simulation.py
@@ -5,6 +5,7 @@ import random
 NUM_TREASURE = 3
 NUM_LOCATIONS = 10
 
+
 def simulate(episodes: int, agent_steps_limit=25):
     treasure_locations = random.sample(range(NUM_LOCATIONS), NUM_TREASURE)
 
@@ -19,13 +20,17 @@ def simulate(episodes: int, agent_steps_limit=25):
     for e in range(1, episodes + 1):
         agent = Agent(treasure_locations, locations)
         while agent.getSteps() <= agent_steps_limit and not agent.atTerminal():
-            print("Agent steps:", agent.getSteps())
-            print("Agent location:", agent.getLocation())
-            print("Agent current reward:", agent.getRewards())
+
+
+            print("Steps: {} | location: {} | current reward: {}".format(agent.getSteps(), agent.getLocation(), agent.getRewards()))
+
+            
             action, reward = agent.randomAction()
             print("Agent", action, "and got", reward, "rewards")
 
         print("History of episode", e)
+        if agent.atTerminal():
+            print("Terminal state reached")
         print("Total reward:", agent.getRewards())
         print("")
         
